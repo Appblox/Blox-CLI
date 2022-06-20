788d142158d4b47f62c4070cfacb97e466b1eb10
Digambaran <arjunsreekumar.1@gmail.com>
2022-06-20 09:43:03 +0530

    chore: license added to subcommands

--- subcommands/connect.js
+++ subcommands/connect.js
@@ -1,5 +1,12 @@
 #!/usr/bin/env node
 
+/**
+ * Copyright (c) Appblox. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 const { Command } = require('commander')
 const { configstore } = require('../configstore')
 const getGithubDeviceCode = require('../utils/getGithubDeviceCode')
--- subcommands/stop-emulator.js
+++ subcommands/stop-emulator.js
@@ -1,4 +1,12 @@
 #!/usr/bin/env node
+
+/**
+ * Copyright (c) Appblox. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 const { stopEmulator } = require('../utils/emulator-manager')
 
 global.rootDir = process.cwd()
