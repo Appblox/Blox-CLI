34af1cbbfc03de75113e5caaca14a759293f1990
Digambaran <arjunsreekumar.1@gmail.com>
2022-06-20 12:01:11 +0530

    chore: license in index and rest

--- utils/__mocks__/registerBlox.js
+++ utils/__mocks__/registerBlox.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (c) Appblox. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
 /* eslint-disable */
 
 module.exports = jest.fn((type, name, ...rest) => {
